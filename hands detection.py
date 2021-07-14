2caef5ff611893e20fef6fd7bbef29928fee3314
Rakeshappasi <83055552+Rakeshappasi@users.noreply.github.com>
2021-07-14 15:55:56 +0530

    Detecting Hands using  mediapipe library

--- hands detection.py
+++ hands detection.py
@@ -4,11 +4,12 @@ import mediapipe as mp
 
 #for drawing
 mp_drawing = mp.solutions.drawing_utils
-# for detecting hands
+# for detecting hands model
 mp_hands = mp.solutions.hands
-
+#for taking cam input
 cap = cv2.VideoCapture(0)
 
+#for  detecting hands
 with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
@@ -22,9 +23,12 @@ with mp_hands.Hands(
     	results = hands.process(image)
     	if results.multi_hand_landmarks:
     		for hand_landmarks in results.multi_hand_landmarks:
+    			#for drawing lines in hands
     			mp_drawing.draw_landmarks(
     				image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    			#for taking output
     			cv2.imshow('MediaPipe Hands', image)
+    	#for closing the output 
     	if cv2.waitKey(10) & 0xFF == ord("q"):
         	break
       
